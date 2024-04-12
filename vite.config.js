b83324f3d8963402dd10d8d54fad0627f8acfdb1
Witty-Wizard <shashankmarch27@gmail.com>
2024-04-12 20:43:50 +0530

    added MUI to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: ""
-})
+  plugins: [reactRefresh()],
+  optimizeDeps: {
+    include: [
+      '@mui/material/Button',
+      '@mui/material/FormControl',
+      '@mui/material/InputLabel',
+      '@mui/material/NativeSelect',
+      '@mui/material/MenuItem',
+    ],
+  },
+  base:""
+});
