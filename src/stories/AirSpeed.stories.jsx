89bc6a24ae7a9fd393fe40744749a10a4c74f5a2
Witty-Wizard <shashankmarch27@gmail.com>
2024-04-23 09:38:57 +0530

    Added Story for AirSpeed

---
+++ src/stories/AirSpeed.stories.jsx
@@ -0,0 +1,30 @@
+import AirSpeed from "../components/InstrumentCluster/AirSpeed/AirSpeed";
+
+export default {
+  title: "AirSpeed",
+  component: AirSpeed,
+};
+
+const Template = (args) => <AirSpeed {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  speed: 50,
+  width: 300,
+  height: 300,
+};
+
+export const ZeroSpeed = Template.bind({});
+ZeroSpeed.args = {
+  speed: 0,
+  width: 300,
+  height: 300,
+};
+
+export const MaxSpeed = Template.bind({});
+MaxSpeed.args = {
+  speed: 160,
+  width: 300,
+  height: 300,
+};
+
