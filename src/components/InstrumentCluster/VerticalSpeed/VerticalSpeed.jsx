5224cf9899970fec88fa9b307c94ec7e27b17953
Witty-Wizard <shashankmarch27@gmail.com>
2024-04-24 17:28:57 +0530

    fixed: VerticalSpeed component

--- src/components/InstrumentCluster/VerticalSpeed/VerticalSpeed.jsx
+++ src/components/InstrumentCluster/VerticalSpeed/VerticalSpeed.jsx
@@ -9,7 +9,7 @@ const VerticalSpeed = ({ verticalSpeed = 0, width = 300, height = 300 }) => {
       <Vertical className="vertical-container" />
       <Needle
         className="needle-container"
-        style={{ transform: `translateY(${verticalSpeed}px)` }}
+        style={{ transform: `rotate(${verticalSpeed}deg)` }}
       />
       <Border className="border-container" />
     </div>
