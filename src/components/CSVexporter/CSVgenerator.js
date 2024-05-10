5288960831e6363a21d46ec75e1e40e85668d447
KUNAL MATHUR <89037807+Kk015@users.noreply.github.com>
2024-05-10 21:26:07 +0530

    chore: Refactor CSVgenerator.js for improved code readability and maintainability

--- src/components/CSVexporter/CSVgenerator.js
+++ src/components/CSVexporter/CSVgenerator.js
@@ -11,17 +11,21 @@ export const appendtoCSV = (newData) => {
   sessionStorage.setItem("csvData", csvContent);
 };
 
+// Function to export the CSV content from session storage as a downloadable file
 export const handleExportCSV = () => {
+  // Get the CSV content from session storage
   const csvData = sessionStorage.getItem("csvData");
+  //  Create a Blob object and download the file if there is CSV data by checking
+  // If there is CSV data
   if (csvData) {
-    const blob = new Blob([csvData], { type: "text/csv" });
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = "Mission Data.csv";
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-    window.URL.revokeObjectURL(url);
+    const blob = new Blob([csvData], { type: "text/csv" }); // Create a new Blob object using the CSV data
+    const url = window.URL.createObjectURL(blob); // Create a URL for the Blob object
+    const a = document.createElement("a"); // Create a new anchor element
+    a.href = url; // Set the href of the anchor to the Blob URL
+    a.download = "Mission Data.csv"; // Set the download attribute of the anchor element to the desired file name
+    document.body.appendChild(a); // Append the anchor element to the body of the document
+    a.click(); // Simulate a click on the anchor element to start the download
+    document.body.removeChild(a); // Remove the anchor element from the body of the document
+    window.URL.revokeObjectURL(url); // Revoke the Blob URL
   }
 };
