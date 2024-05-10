d73d9ea57ee0e0f1fd53ba16b5923ac74af1e069
KUNAL MATHUR <89037807+Kk015@users.noreply.github.com>
2024-05-10 21:54:34 +0530

    refactor: Update baudrateSlice and connectedSlice reducers

--- src/features/configs/connectedSlice.jsx
+++ src/features/configs/connectedSlice.jsx
@@ -1,13 +1,17 @@
+// Set the state value to the payload of the action
 import { createSlice } from "@reduxjs/toolkit";
 
+// Create a slice of the Redux store for the connection status
+// A slice includes a reducer, actions, and selectors
 export const connectedSlice = createSlice({
-  name: "connected",
+  name: "connected", // The name of the slice
   initialState: {
-    value: false,
+    value: false, // The initial state value, indicating not connected
   },
   reducers: {
+    // Define a reducer for setting the connection status
     setConnected: (state, action) => {
-      state.value = action.payload;
+      state.value = action.payload; // Set the state value to the payload of the action
     },
   },
 });
