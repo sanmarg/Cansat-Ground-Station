17a12b230d70f42a58a09ffe132fb308635b2278
KUNAL MATHUR <89037807+Kk015@users.noreply.github.com>
2024-05-10 23:15:48 +0530

    chore: Update Storybook configuration and preview files

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,21 @@
+// Import the Preview type from '@storybook/react'
 /** @type { import('@storybook/react').Preview } */
+
+// Define the preview configuration
 const preview = {
   parameters: {
     controls: {
       matchers: {
+        // Define a regex matcher for color controls
+        // This will match any control that ends with 'background' or 'color' (case insensitive)
         color: /(background|color)$/i,
+        // Define a regex matcher for date controls
+        // This will match any control that ends with 'Date' (case insensitive)
         date: /Date$/i,
       },
     },
   },
 };
 
+// Export the preview configuration as the default export
 export default preview;
